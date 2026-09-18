44e3f9fd58b6511b7a7633051bf5ac672d5f2336
Subhankar733 <subhankar733@users.noreply.github.com>
2026-09-18 11:30:30 +0530

    fix: import mutableStateListOf

--- app/src/main/java/com/subho/olikh/browser/MainActivity.kt
+++ app/src/main/java/com/subho/olikh/browser/MainActivity.kt
@@ -78,6 +78,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
